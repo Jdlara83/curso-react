e9f5f3529e7f884990b43841583b67f3748d60a4
Jdlara83 <37850267+Jdlara83@users.noreply.github.com>
2022-10-01 18:59:31 -0500

    comenzando con react freecodecamp

---
+++ src/components/Testimonio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Testimonio() {
+  return (
+    <div className='contenedor-testimonio'>
+      <img className='imagen-testimonio' src={require('../img/testimonio-emma.png')} alt='foto-Emma' />
+      <div className='text-testimonio'>
+        <p className='nom-test'>Emma Bostian en Suecia</p>
+        <p className='car-test'>Ingeniera de Software en Spotify </p>
+        <p className='nom-test'>'Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
